b0bbdf65471a0ab226ce1afa118dc31179291066
Andrej Petras <andrej@lorislab.org>
2024-03-14 10:41:21 +0100

    feat: native build

--- src/main/java/org/tkit/onecx/workspace/domain/models/AssignmentMenu.java
+++ src/main/java/org/tkit/onecx/workspace/domain/models/AssignmentMenu.java
@@ -1,4 +1,7 @@
 package org.tkit.onecx.workspace.domain.models;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
 public record AssignmentMenu(String menuItemId, String roleName) {
 }
