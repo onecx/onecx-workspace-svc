9c334275284581c430f9e1f72627ad89ac0a7374
JordenReuter <149687553+JordenReuter@users.noreply.github.com>
2024-07-12 12:12:20 +0200

    feat: added disabled flag (#104)
    * feat: added disabled flag
    
    * feat: display name for shell

--- src/main/java/org/tkit/onecx/workspace/domain/di/mappers/TemplateMapper.java
+++ src/main/java/org/tkit/onecx/workspace/domain/di/mappers/TemplateMapper.java
@@ -45,6 +45,7 @@ public interface TemplateMapper {
         return assignments;
     }
 
+    @Mapping(target = "disabled", ignore = true)
     @Mapping(target = "mandatory", constant = "true")
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "theme", source = "themeName")
