9e02e65ec74862456069bbe87af191904906ff6a
Jannik Steenken <jannik.steenken@capgemini.com>
2024-07-17 11:31:54 +0200

    feat: add interpolate test userMenu url

---
+++ src/test/java/org/tkit/onecx/workspace/rs/user/controllers/UserMenuInternalControllerInterpolateTest.java
@@ -0,0 +1,47 @@
+package org.tkit.onecx.workspace.rs.user.controllers;
+
+import gen.org.tkit.onecx.workspace.rs.user.model.UserWorkspaceMenuRequestDTO;
+import gen.org.tkit.onecx.workspace.rs.user.model.UserWorkspaceMenuStructureDTO;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+import org.tkit.onecx.workspace.test.AbstractTest;
+import org.tkit.quarkus.security.test.GenerateKeycloakClient;
+import org.tkit.quarkus.test.WithDBData;
+
+import static io.restassured.RestAssured.given;
+import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
+import static jakarta.ws.rs.core.Response.Status.OK;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.tkit.quarkus.security.test.SecurityTestUtils.getKeycloakClientToken;
+
+@QuarkusTest
+@TestHTTPEndpoint(UserMenuInternalController.class)
+@WithDBData(value = "data/testdata-user.xml", deleteBeforeInsert = true, deleteAfterTest = true, rinseAndRepeat = true)
+@GenerateKeycloakClient(clientName = "testClient", scopes = { "ocx-ws:all", "ocx-ws:read", "ocx-ws:write" })
+public class UserMenuInternalControllerInterpolateTest extends AbstractTest {
+
+    @Test
+    void getMenuStructure_shouldReturnSystemURL() {
+
+        var workspaceName = "test03";
+        var accessToken = createAccessTokenBearer(USER_BOB);
+        var idToken = createToken("org1");
+
+        var data = given()
+                .auth().oauth2(getKeycloakClientToken("testClient"))
+                .when()
+                .contentType(APPLICATION_JSON)
+                .header(APM_HEADER_PARAM, idToken)
+                .body(new UserWorkspaceMenuRequestDTO().token(accessToken))
+                .pathParam("workspaceName", workspaceName)
+                .post()
+                .then()
+                .statusCode(OK.getStatusCode())
+                .extract().body().as(UserWorkspaceMenuStructureDTO.class);
+
+        assertThat(data).isNotNull();
+        assertThat(data.getMenu().get(1).getChildren().get(0).getUrl()).isEqualTo("/company3/testItem1");
+    }
+
+}
