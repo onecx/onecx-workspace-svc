9e90fdf0512ed88a65d5cdba8e029e78accc5b4a
JordenReuter <149687553+JordenReuter@users.noreply.github.com>
2024-12-05 08:06:00 +0100

    feat: added role based menu tree (#138)

--- src/main/java/org/tkit/onecx/workspace/rs/internal/mappers/MenuItemMapper.java
+++ src/main/java/org/tkit/onecx/workspace/rs/internal/mappers/MenuItemMapper.java
@@ -1,10 +1,9 @@
 package org.tkit.onecx.workspace.rs.internal.mappers;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
+import org.apache.commons.text.StringSubstitutor;
 import org.mapstruct.*;
 import org.tkit.onecx.workspace.domain.criteria.MenuItemLoadCriteria;
 import org.tkit.onecx.workspace.domain.criteria.MenuItemSearchCriteria;
@@ -141,5 +140,68 @@ public interface MenuItemMapper {
         menuItem.setWorkspace(workspace);
         menuItem.setPosition(position);
         menuItem.setParent(parent);
+    }
+
+    default MenuItemStructureDTO mapTreeByRoles(Workspace workspace, Collection<MenuItem> menuItems,
+            Map<String, Set<String>> mapping,
+            HashSet<String> roles) {
+        MenuItemStructureDTO dto = new MenuItemStructureDTO();
+
+        if (menuItems.isEmpty()) {
+            return dto;
+        }
+        Set<MenuItem> items;
+
+        items = filterMenu(new HashSet<>(menuItems), mapping, roles, workspace.getBaseUrl());
+        if (items.isEmpty()) {
+            return dto;
+        }
+
+        return dto.menuItems(items.stream().map(this::mapTreeItem).toList());
+    }
+
+    default Set<MenuItem> filterMenu(Set<MenuItem> items, Map<String, Set<String>> mapping, Set<String> roles,
+            String workspaceUrl) {
+        Set<MenuItem> tmp = new HashSet<>(items);
+        final var sub = new StringSubstitutor(System.getenv());
+        tmp.forEach(rootItem -> {
+            var mr = mapping.get(rootItem.getId());
+            if (mr == null || mr.stream().noneMatch(roles::contains)) {
+                items.remove(rootItem);
+            } else {
+                if (rootItem.getChildren() != null && !rootItem.getChildren().isEmpty()) {
+                    filterChildren(rootItem, mapping, roles, workspaceUrl, sub);
+                } else {
+                    rootItem.setUrl(updateInternalUrl(workspaceUrl, rootItem.getUrl(), rootItem.isExternal(), sub));
+                }
+            }
+        });
+
+        return items;
+    }
+
+    default void filterChildren(MenuItem menuItem, Map<String, Set<String>> mapping, Set<String> roles,
+            String workspaceUrl, StringSubstitutor sub) {
+        Set<MenuItem> items = new HashSet<>(menuItem.getChildren());
+        items.forEach(child -> {
+            var mr = mapping.get(child.getId());
+            if (mr == null || mr.stream().noneMatch(roles::contains)) {
+                menuItem.getChildren().remove(child);
+            } else {
+                if (child.getChildren() != null && !child.getChildren().isEmpty()) {
+                    filterChildren(child, mapping, roles, workspaceUrl, sub);
+                } else {
+                    child.setUrl(updateInternalUrl(workspaceUrl, child.getUrl(), child.isExternal(), sub));
+                }
+            }
+        });
+    }
+
+    default String updateInternalUrl(String workspaceUrl, String menuItemUrl, Boolean isExternal, StringSubstitutor sub) {
+        if (Boolean.TRUE.equals(isExternal)) {
+            return sub.replace(menuItemUrl);
+        } else {
+            return sub.replace(workspaceUrl + menuItemUrl);
+        }
     }
 }
