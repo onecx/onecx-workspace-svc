78f94a23646013ca25943acf1e992355648b4867
Andrej Petras <andrej@lorislab.org>
2025-05-22 09:50:19 +0200

    feat: remove legacy tkit-portal api

--- src/test/java/org/tkit/onecx/workspace/AfterStartDataImportTest.java
+++ src/test/java/org/tkit/onecx/workspace/AfterStartDataImportTest.java
@@ -3,7 +3,6 @@ package org.tkit.onecx.workspace;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import jakarta.inject.Inject;
 
@@ -11,7 +10,6 @@ import org.junit.jupiter.api.Test;
 import org.tkit.onecx.workspace.domain.daos.AssignmentDAO;
 import org.tkit.onecx.workspace.domain.daos.WorkspaceDAO;
 import org.tkit.onecx.workspace.domain.models.Role;
-import org.tkit.onecx.workspace.domain.models.Workspace;
 import org.tkit.onecx.workspace.test.AbstractTest;
 
 import io.quarkus.test.junit.QuarkusTest;
