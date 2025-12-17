51e351f7f669a651da104916993e3e938d5eafae
Andrej Petras <andrej@lorislab.org>
2025-12-17 12:53:20 +0100

    chore(deps): update quarkus to 3.27 lts version (#214)

--- src/main/java/org/tkit/onecx/workspace/rs/user/services/MenuMappingConfig.java
+++ src/main/java/org/tkit/onecx/workspace/rs/user/services/MenuMappingConfig.java
@@ -5,11 +5,9 @@ import java.util.Map;
 import io.quarkus.runtime.annotations.ConfigDocFilename;
 import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
-import io.quarkus.runtime.annotations.StaticInitSafe;
 import io.smallrye.config.ConfigMapping;
 import io.smallrye.config.WithName;
 
-@StaticInitSafe
 @ConfigMapping(prefix = "onecx.workspace")
 @ConfigDocFilename("onecx-workspace-svc.adoc")
 @ConfigRoot(phase = ConfigPhase.RUN_TIME)
--- src/main/java/org/tkit/onecx/workspace/rs/user/services/TokenConfig.java
+++ src/main/java/org/tkit/onecx/workspace/rs/user/services/TokenConfig.java
@@ -5,12 +5,10 @@ import java.util.Optional;
 import io.quarkus.runtime.annotations.ConfigDocFilename;
 import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
-import io.quarkus.runtime.annotations.StaticInitSafe;
 import io.smallrye.config.ConfigMapping;
 import io.smallrye.config.WithDefault;
 import io.smallrye.config.WithName;
 
-@StaticInitSafe
 @ConfigMapping(prefix = "onecx.workspace")
 @ConfigDocFilename("onecx-workspace-svc.adoc")
 @ConfigRoot(phase = ConfigPhase.RUN_TIME)
