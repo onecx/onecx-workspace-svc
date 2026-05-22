a7a482dbd12c823eb3541e0ffbd5f86846bcb3b2
Jorden_Reuter <149687553+JordenReuter@users.noreply.github.com>
2026-05-22 14:22:50 +0200

    feat: ned endpoint to simplify slot management

--- src/main/java/org/tkit/onecx/workspace/domain/services/SlotService.java
+++ src/main/java/org/tkit/onecx/workspace/domain/services/SlotService.java
@@ -10,12 +10,19 @@ import jakarta.transaction.Transactional;
 import org.tkit.onecx.workspace.domain.daos.SlotDAO;
 import org.tkit.onecx.workspace.domain.models.Component;
 import org.tkit.onecx.workspace.domain.models.Slot;
+import org.tkit.onecx.workspace.domain.models.Workspace;
+import org.tkit.onecx.workspace.rs.internal.mappers.SlotMapper;
+
+import gen.org.tkit.onecx.workspace.rs.internal.model.UpdateSlotRequestDTO;
 
 @ApplicationScoped
 public class SlotService {
 
     @Inject
     SlotDAO dao;
+
+    @Inject
+    SlotMapper mapper;
 
     @Transactional
     public Slot updateWithExistingComponents(Slot slot, List<Component> components) {
@@ -29,5 +36,33 @@ public class SlotService {
     public Slot updateWithoutComponents(Slot slot, List<Component> components) {
         slot.setComponents(components);
         return dao.update(slot);
+    }
+
+    public Slot createOrUpdateSlot(Workspace workspace, UpdateSlotRequestDTO updateSlotRequestDTO) {
+        var existingSlots = dao.findSlotsByWorkspaceId(workspace.getId());
+        var matchingSlot = existingSlots.stream().filter(slot -> slot.getName().equals(updateSlotRequestDTO.getName()))
+                .findFirst();
+        Slot updatedOrCreatedSlot;
+
+        //slot exists
+        if (matchingSlot.isPresent()) {
+            // slot components are empty, delete the slot
+            if (updateSlotRequestDTO.getComponents() == null || updateSlotRequestDTO.getComponents().isEmpty()) {
+                dao.deleteById(matchingSlot.get().getId());
+                return null;
+            }
+            if (matchingSlot.get().getComponents().isEmpty()) {
+                updatedOrCreatedSlot = updateWithoutComponents(matchingSlot.get(),
+                        mapper.mapComponents(updateSlotRequestDTO.getComponents()));
+            } else {
+                updatedOrCreatedSlot = updateWithExistingComponents(matchingSlot.get(),
+                        mapper.mapComponents(updateSlotRequestDTO.getComponents()));
+            }
+            //slot does not exist, create new slot with components
+        } else {
+            updatedOrCreatedSlot = mapper.create(updateSlotRequestDTO, workspace);
+            updatedOrCreatedSlot = dao.create(updatedOrCreatedSlot);
+        }
+        return updatedOrCreatedSlot;
     }
 }
